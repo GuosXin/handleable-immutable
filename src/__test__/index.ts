badaec83c2e507e76dd0f40fdf9c1c387beba4c5
GuosXin <2478930890@qq.com>
2023-05-24 20:44:23 +0800

    实现先收集草稿再拷贝

--- src/__test__/index.ts
+++ src/__test__/index.ts
@@ -13,11 +13,12 @@ const state = {
         proxy: 1
     },
 }
-const immer = createImmutable(state)
-immer.a.b.push(123)
-immer.a.c.d.f = 666
-immer.a.c=immer.a
-console.log(state, getImmutableCopy(immer))
+// const immer = createImmutable(state)
+// // immer.a.b.push(123)
+// // immer.a.c.d.f = 666
+// immer.a.c = immer.a
+// const copy = getImmutableCopy(immer)
+// console.log(state, copy)
 
 
 
@@ -60,6 +61,7 @@ console.log(state, getImmutableCopy(immer))
 // immer.a = 1
 // console.log(immer, getImmutableCopy(immer), state)
 
+
 // console.time('create')
 // const dataSource = Object.fromEntries(
 //   [...Array(4000).keys()].map(key => [key, {key, data: {value: key}}])
@@ -70,5 +72,22 @@ console.log(state, getImmutableCopy(immer))
 // console.time('update');
 // const proxy = createImmutable(dataSource)
 // proxy[1000].data.value = 100;
-// getClone(proxy)
+// getImmutableCopy(proxy)
 // console.timeEnd('update');
+
+
+
+console.time('create')
+const dataSource = Object.fromEntries(
+  [...Array(4000).keys()].map(key => [key, {key, data: {value: key}}])
+)
+console.log(dataSource)
+console.timeEnd('create')
+
+console.time('update');
+const proxy = createImmutable(dataSource)
+for(let i = 0; i < 1000000; i++){
+  proxy[1000].data.value = i;
+}
+getImmutableCopy(proxy)
+console.timeEnd('update');
--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,6 @@ export let createImmutable: CreateImmutable = function(base, handler = { set: ()
     const source = {
         isImmutable: true,
         base,
-        // copy: parent.receiver ? parent.receiver[BASE] : null,
         copy: base,
         parent: parent.receiver,
         prop: parent.prop,
@@ -78,33 +77,28 @@ export let createImmutable: CreateImmutable = function(base, handler = { set: ()
         get: function(target, prop, receiver){
             // 匹配target的prop属性
             let res = matchTargetProp(target, prop)
+            // 匹配到则返回对应的值
             if(res.match){
                 return res.value
             }
             // 执行getter
-            receiver[HANDLER] && receiver[HANDLER].get && receiver[HANDLER].get(target, prop, receiver)
+            // receiver[HANDLER] && receiver[HANDLER].get && receiver[HANDLER].get(target, prop, receiver)
             return Reflect.get(target.proxy, prop, receiver)
         },
         set: function(target, prop, newValue, receiver){
             if(prop === HANDLER){
                 return Reflect.set(target, 'handler', newValue)
             }
-            // 值为不可变数据类型时，需要转换
-            newValue = getImmutableCopy(newValue)
+            if(prop === SETLOG){
+                return Reflect.set(target, 'setLog', newValue)
+            }
             if(prop === COPY){
                 return Reflect.set(target, 'copy', newValue)
             }
-            // 进行拷贝
-            // receiver[COPY][prop] = newValue
-            // let obj = receiver
-            // while(obj[PARENT]){
-            //     obj[PARENT][COPY] = Object.assign(obj[PARENT][COPY], { [obj[PROP]]: obj[COPY] })
-            //     obj = obj[PARENT]
-            // }
+            // 记录草稿
             receiver[SETLOG].push({receiver, prop, newValue})
-            // console.log(receiver)
             // 执行setter
-            receiver[HANDLER] && receiver[HANDLER].set && receiver[HANDLER].set(target, prop, newValue, receiver)
+            // receiver[HANDLER] && receiver[HANDLER].set && receiver[HANDLER].set(target, prop, newValue, receiver)
             return Reflect.set(target.proxy, prop, newValue, target.proxy)
         }
     })
@@ -175,30 +169,60 @@ function matchTargetProp(target: any, prop: string | symbol){
 export function getImmutableCopy(proxy: any){
     if(proxy[ISIMMUTABLE]){
         let setLog = proxy[SETLOG]
+        if(setLog.length === 0) {
+            return proxy[COPY]
+        }
         setLog.forEach((item: SetLogType) => {
             const { receiver, prop, newValue } = item
             // 路径拷贝
-            let r = receiver
-            let lock = true
-            while(r){
-                let copy = shallowCopy(r[COPY])
-                // 上锁，只有叶子节点需要赋值
-                if(lock){
-                    copy[prop] = newValue
-                    lock = false
-                }
-                r[COPY] = copy
-                // 向上拷贝
-                if(r[PARENT]){
-                    r[PARENT][COPY] = shallowCopy(r[PARENT][COPY])
-                    r[PARENT][COPY][r[PROP]] = r[COPY]
-                }
-                r = r[PARENT]
+            let r = copyPath(receiver)
+            // 叶子节点赋值
+            if(newValue && newValue[ISIMMUTABLE]){
+                r[COPY][prop] = newValue[COPY]
+            }else{
+                r[COPY][prop] = newValue
             }
         })
+        setLog.length = 0
         return proxy[COPY]
     }
     return proxy
+}
+
+/**
+ * 路径拷贝
+ * @param receiver 
+ * @returns 
+ */
+function copyPath(receiver: any){
+    // 如果已经到根节点，提前结束
+    if(
+        !receiver || 
+        (!receiver[PARENT] && receiver[COPY] !== receiver[BASE])
+    ){
+        return receiver
+    }
+    // 如果已经拷贝过了，提前结束
+    if(
+        receiver[COPY] !== receiver[BASE] &&
+        receiver[PARENT] &&
+        receiver[PARENT][COPY] !== receiver[PARENT][BASE]
+    ){
+        receiver[PARENT][COPY][receiver[PROP]] = receiver[COPY]
+        return receiver
+    }
+    // 拷贝叶子节点
+    let r = receiver
+    if(r[COPY] === r[BASE]){
+        r[COPY] = shallowCopy(r[COPY])
+    }
+    // 向上拷贝
+    if(r[PARENT]){
+        r[PARENT][COPY] = shallowCopy(r[PARENT][COPY])
+        r[PARENT][COPY][r[PROP]] = r[COPY]
+    }
+    copyPath(r[PARENT])
+    return receiver
 }
 
 // /**
